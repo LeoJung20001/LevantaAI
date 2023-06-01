fd8feca7b00413f147ba2c1b0cb97ef2c06757cc
leonardo <jungmanleo69@gmail.com>
2023-06-01 17:02:22 -0300

    Primeiro Comit do app

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
 
-export default function App() {
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+    <Text style={styles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <Text style={styles.textStyle}>{"Ola mundo do Jung!"}</Text>
+      <AppButton title="Clica ni mim" size="sm" backgroundColor="#007bff"/>
+      <AppButton title="Mehh" size="sm" backgroundColor="#007bff"/>
+
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "top",
+    padding: 16
   },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  textStyle:{
+    fontSize: 20,
+    color: "eb4034",
+    textAlign: "center",
+    marginBottom: 20
+}
 });
+
+export default App;
